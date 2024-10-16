b52ad1d44e1f5692e180b8f07c1981ce924ae7c6
Artem_PC <taftoos2@gmail.com>
2024-10-16 19:51:03 +0300

    feat: all tests passed

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.array=[];
+        this.middleIndex= null;
+    }
 
     setRange(min, max) {
-
+        for (let i = 0; i < max-min; i++) {
+            this.array.push(i);
+        }
     }
 
     guess() {
-
+        this.middleIndex =  Math.round(this.array.length / 2);
+        // console.log(this.array);
+        return this.array[this.middleIndex];
     }
 
     lower() {
-
+        this.array= this.array.slice(0, this.middleIndex);
     }
 
     greater() {
-
+        this.array= this.array.slice(this.middleIndex, this.array.length);
     }
 }
 
